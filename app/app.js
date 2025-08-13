7b6393cd06340f4629fb5711f2bb7f5ddcf42608
AlexTH23 <2236000174@utna.edu.mx>
2025-08-12 19:45:14 -0600

    el ese era en el require xd

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // Importar rutas
 const router = require('./routes/librosRoute');
 const usuarioRoutes = require('./routes/usuarioRoute');
-const authRoutes = require('./routes/authRoutes'); // Ahora sí existe este archivo
+const authRoutes = require('./routes/authRoute'); // Ahora sí existe este archivo
 
 // Middlewares para parsear datos
 app.use(express.urlencoded({ extended: false }));
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Rutas
 app.use('/libros', router);
-app.use('/auth', authRoute);      // Ruta /auth funcionando
+app.use('/auth', authRoutes);      // Ruta /auth funcionando
 app.use('/usuarios', usuarioRoutes);
 
 module.exports = app;
